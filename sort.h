0197bac230b934028bbf06fe0f7d3f2d6129f1d4
M9china <nqobilencaza2@gmail.com>
2023-10-30 09:58:17 -0700

    compiled code

--- sort.h
+++ sort.h
@@ -3,6 +3,7 @@
 
 #include <stddef.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  * struct listint_s - Doubly linked list node
@@ -28,5 +29,6 @@ void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
 void swap_nodes(listint_t **list, listint_t **node1, listint_t **node2);
 void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
 
 #endif
